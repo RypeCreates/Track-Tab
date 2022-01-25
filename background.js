0ab2e8e1d1c1a26f5c4a4a5aad94709fda1225e4
Malik Shalash <mjshalash@rci4u.com>
2022-01-24 21:51:00 -0500

    favicon-alter:     Working to alter favIconURL property of retrieved tab

--- background.js
+++ background.js
@@ -1,6 +1,17 @@
-let color = '#3aa757';
+change_favicon = async (img) => {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions, async (tab) => {
+    await chrome.tabs.update(tab.id, { favIconUrl: "" }, () => {
+      console.log(tab);
+    });
+  });
+};
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+let color = "#3aa757";
+
+//chrome.runtime.onInstalled.addListener(() => {
+//chrome.storage.sync.set({ color });
+//console.log("Default background color set to %cgreen", `color: ${color}`);
+
+change_favicon("https://codepad.co/img/icn_logo.png");
+//});
