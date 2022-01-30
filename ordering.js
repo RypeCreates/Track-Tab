1d3d8cba4d04d6fd35a7406a7264c72ae08bd26a
Ryan Petit <petit.ryank@gmail.com>
2022-01-30 11:26:55 -0500

    Navigation Constructor now stores a complete doubly linked list of open tabs, linked left to right

--- ordering.js
+++ ordering.js
@@ -3,41 +3,49 @@ class Navigation {
 
     constructor() 
     {
-        this.initializeTabList((currentTab, tabsList) => {
+        this.initializeTabList((queriedTabs) => {
+            // find the current tab in the queried tabsList 
+            let currentTab = queriedTabs.find(function(tab) {
+                return tab.active == true;
+            });
+
             this.currentTab = new TrackTab(currentTab);
-            console.log("Current Tab: " + JSON.stringify(this.currentTab));
-    
             this.tabsList = new Array();
 
             let prevTab = null;
-            for(let tab in tabsList)
-            {
-                let newTab = new TrackTab(tab);
-                newTab.prev = prevTab;
-                this.tabsList.push(tab);    
-                prevTab = newTab;
-            }
-            
-            console.log("Tabs List: " + JSON.stringify(this.tabsList));
 
-            this.getAllTabsAsync
-        })
+            queriedTabs.forEach(t => {
+                let newTab = new TrackTab(t);
+                
+                // TODO this could probably be refactored eventually
+                if(prevTab){
+                    newTab.prevId = prevTab.tab.id;
+                    prevTab.nextId = newTab.tab.id;
+                } 
+                this.tabsList.push(newTab);  
+                prevTab = newTab;
+            })
+
+            // close the loop
+            this.tabsList[this.tabsList.length - 1].nextId = this.tabsList[0].tab.id;
+            this.tabsList[0].prevId = this.tabsList[this.tabsList.length - 1].tab.id;
+
+            // TODO add a conditional to only log while in sandbox
+            console.log("(constructor) tabsList: " + JSON.stringify(this.tabsList));
+        });
     }
 
-    // TODO this currently just queries the FIRST tab
     async getAllTabsAsync() {
         let queryOptions = {};
-        let [tabs] = await chrome.tabs.query(queryOptions);
-        
-        console.log("tabsList:" + JSON.stringify(tabs));
+        let tabs = await chrome.tabs.query(queryOptions);
         
         return tabs;
     }
 
+    // TODO this can probably just be combined with getAllTabsAsync
     async initializeTabList(callback) {
-        let currentTab = await this.getCurrentTabAsync();
         let tabsList = await this.getAllTabsAsync();
-        callback(currentTab, tabsList);
+        callback(tabsList);
     }
     
     // Gets the tab that this script call is being made from. 
@@ -46,7 +54,6 @@ class Navigation {
         debugger;
         let queryOptions = { active: true, currentWindow: true };
         let [tab] = await chrome.tabs.query(queryOptions);
-        console.log("tab:" + JSON.stringify(tab));
         return tab;
     }
 }
@@ -55,8 +62,8 @@ class TrackTab {
     constructor(tab)
     {
         this.tab = tab;
-        this.prev = null;
-        this.next = null;  
+        this.prevId = null;
+        this.nextId = null;  
     }
 }
 
