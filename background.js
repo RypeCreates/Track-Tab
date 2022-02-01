cd01b22b4280d4853733e61c4c68f20b4d70653e
Malik Shalash <mjshalash@rci4u.com>
2022-01-31 22:14:21 -0500

    Started work on image selector on sandbox/options page

--- background.js
+++ background.js
@@ -6,3 +6,10 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log("Default background color set to %cgreen", `color: ${color}`);
 });
+
+// IN PROGRESS but query can be used to keep track of # of tabs in a given window and track accordingly
+chrome.tabs.onCreated.addListener(() => {
+  chrome.tabs.query({ currentWindow: true }, (count) => {
+    console.log("# of Tabs: ", count.length);
+  });
+});
--- content-scripts.js
+++ content-scripts.js
@@ -1,7 +1,8 @@
-var imageLink = chrome.runtime.getURL("./images/kirby.png");
+var imageOneLink = chrome.runtime.getURL("/images/tab_images/kirby.png");
+
 var link =
   document.querySelector("link[rel*='icon']") || document.createElement("link");
 link.type = "image/x-icon";
 link.rel = "shortcut icon";
-link.href = imageLink;
+link.href = imageOneLink;
 document.getElementsByTagName("head")[0].appendChild(link);
